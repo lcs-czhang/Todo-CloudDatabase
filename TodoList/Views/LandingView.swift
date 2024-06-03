08e2f249bb60bef0aab03ac5ff6525aa8cc65b4d
Youzhou Zhang <czhang@lcs.on.ca>
2024-06-03 15:26:24 -0400

    Removed user interface for adding a new to-do item from the main landing view of the app.

--- TodoList/Views/LandingView.swift
+++ TodoList/Views/LandingView.swift
@@ -11,9 +11,6 @@ struct LandingView: View {
     
     // MARK: Stored properties
     
-    // The item currently being added
-    @State var newItemDescription = ""
-    
     // The search text
     @State var searchText = ""
     
@@ -49,19 +46,6 @@ struct LandingView: View {
                         try await viewModel.filterTodos(on: searchText)
                     }
                 }
-                
-                HStack {
-                    TextField("Enter a to-do item", text: $newItemDescription)
-                    
-                    Button("ADD") {
-                        // Add the new to-do item
-                        viewModel.createToDo(withTitle: newItemDescription)
-                        newItemDescription = ""
-                    }
-                    .font(.caption)
-                    .disabled(newItemDescription.trimmingCharacters(in: .whitespaces).isEmpty == true)
-                }
-                .padding(20)
                 
             }
             .navigationTitle("To do")
